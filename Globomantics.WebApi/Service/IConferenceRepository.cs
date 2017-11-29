00164db006deea402415fa002bb7516cb8708c97
tech-guru42 <web.dev4285@gmail.com>
2017-11-29 22:48:13 +0530

    Preparing skeleton of Web API.

---
+++ Globomantics.WebApi/Service/IConferenceRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Globomantics.WebApi.Service
+{
+    public interface IConferenceRepository
+    {
+        Task<IEnumerable<ConferenceModel>> GetAll();
+
+        Task<ConferenceModel> GetById(int id);
+
+        Task<StatisticsModel> GetStatistics();
+
+        Task Add(ConferenceModel model);
+    }
+}
---
+++ Globomantics.WebApi/Service/IProposalRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Globomantics.WebApi.Service
+{
+    public interface IProposalRepository
+    {
+        Task Add(ProposalModel model);
+
+        Task<IEnumerable<ProposalModel>> GetAll(int conferenceId);
+
+        Task<ProposalModel> Approve(int proposalId);
+    }
+}
